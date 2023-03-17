659e7c6e9daae07785e9d51c6b30df877de43fe0
jasoworld <jasoworld@hotmail.com>
2023-03-17 14:40:48 +0800

    19 Render an R script

---
+++ foo.R
@@ -0,0 +1,20 @@
+# ---
+# title: "Untitled"
+# author: "jasoworld"
+# date: "2023-03-17"
+# output: html_document
+# ---
+
+
+## R Markdown
+
+
+## Including Plots
+
+# You can also embed plots, for example:
+#
+# ```{r pressure, echo=FALSE}
+plot(pressure)
+# ```
+#
+# Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
